09db1a6203d222665f6be0c34f3cced2732c9e7c
Paul Black <pblack67@comcast.net>
2019-07-25 11:46:07 -0500

    Added Star Trek Memory Game project

--- models/projects.js
+++ models/projects.js
@@ -1,81 +1,88 @@
 let projects = [
-    {
-      title: "Word Guessing Game",
-      href: "https://pblack67.github.io/WordGuessGame/",
-      image: "assets/images/bears_logo2.png",
-      alt: "Go Bears"
-    },
-    {
-      title: "Star Wars RPG",
-      href: "https://pblack67.github.io/StarWarsGame/",
-      image: "assets/images/luke-skywalker2.jpeg",
-      alt: "Star Wars RPG"
-    },
-    {
-      title: "Opera Trivia Game",
-      href: "https://pblack67.github.io/OperaTriviaGame/",
-      image: "assets/images/ring2.jpg",
-      alt: "Wagner's Ring"
-    },
-    {
-      title: "My Favorite Things",
-      href: "https://pblack67.github.io/MyFavoriteThings/",
-      image: "assets/images/julie-andrews.jpg",
-      alt: "My Favorite Things"
-    },
-    {
-      title: "Online Rock, Paper, Scissors",
-      href: "https://pblack67.github.io/HW7-RPS-Multiplayer/",
-      image: "assets/images/rock-paper-scissors.jpg",
-      alt: "Rock, Paper, Scissors"
-    },
-    {
-      title: "The Binger",
-      href: "https://team1-rikafaraahpaul.github.io/Aggregator/index.html",
-      image: "assets/images/tablet.jpg",
-      alt: "The Binger"
-    },
-    {
-      title: "Liri",
-      href: "https://github.com/pblack67/HW8-Liri-Node-App",
-      image: "assets/images/cltool.jpg",
-      alt: "Liri"
-    },
-    {
-      title: "CLI Word Guess Game",
-      href: "https://github.com/pblack67/HW9-CLIWordGuessGame",
-      image: "assets/images/cltool.jpg",
-      alt: "CLIWordGuessGame"
-    },
-    {
-      title: "Bamazon Storefront",
-      href: "https://github.com/pblack67/HW10-Bamazon",
-      image: "assets/images/cltool.jpg",
-      alt: "Bamazon"
-    },
-    {
-      title: "Friend Finder",
-      href: "https://friendfinder-peb.herokuapp.com/",
-      image: "assets/images/friends.jpg",
-      alt: "FriendFinder"
-    },
-    {
-      title: "Eat Da Burger",
-      href: "https://eatdaburger-peb.herokuapp.com/",
-      image: "assets/images/burger.jpg",
-      alt: "Eat Da Burger"
-    },
-    {
-      title: "Refassign",
-      href: "https://refassign.herokuapp.com/",
-      image: "assets/images/referee.jpg",
-      alt: "Refassign"
-    },
-    {
-      title: "Scraper Deluxe",
-      href: "https://scraper-peb.herokuapp.com/",
-      image: "assets/images/scraper.jpg",
-      alt: "Scraper Deluxe"
-    }  ];
+  {
+    title: "Word Guessing Game",
+    href: "https://pblack67.github.io/WordGuessGame/",
+    image: "assets/images/bears_logo2.png",
+    alt: "Go Bears"
+  },
+  {
+    title: "Star Wars RPG",
+    href: "https://pblack67.github.io/StarWarsGame/",
+    image: "assets/images/luke-skywalker2.jpeg",
+    alt: "Star Wars RPG"
+  },
+  {
+    title: "Opera Trivia Game",
+    href: "https://pblack67.github.io/OperaTriviaGame/",
+    image: "assets/images/ring2.jpg",
+    alt: "Wagner's Ring"
+  },
+  {
+    title: "My Favorite Things",
+    href: "https://pblack67.github.io/MyFavoriteThings/",
+    image: "assets/images/julie-andrews.jpg",
+    alt: "My Favorite Things"
+  },
+  {
+    title: "Online Rock, Paper, Scissors",
+    href: "https://pblack67.github.io/HW7-RPS-Multiplayer/",
+    image: "assets/images/rock-paper-scissors.jpg",
+    alt: "Rock, Paper, Scissors"
+  },
+  {
+    title: "The Binger",
+    href: "https://team1-rikafaraahpaul.github.io/Aggregator/index.html",
+    image: "assets/images/tablet.jpg",
+    alt: "The Binger"
+  },
+  {
+    title: "Liri",
+    href: "https://github.com/pblack67/HW8-Liri-Node-App",
+    image: "assets/images/cltool.jpg",
+    alt: "Liri"
+  },
+  {
+    title: "CLI Word Guess Game",
+    href: "https://github.com/pblack67/HW9-CLIWordGuessGame",
+    image: "assets/images/cltool.jpg",
+    alt: "CLIWordGuessGame"
+  },
+  {
+    title: "Bamazon Storefront",
+    href: "https://github.com/pblack67/HW10-Bamazon",
+    image: "assets/images/cltool.jpg",
+    alt: "Bamazon"
+  },
+  {
+    title: "Friend Finder",
+    href: "https://friendfinder-peb.herokuapp.com/",
+    image: "assets/images/friends.jpg",
+    alt: "FriendFinder"
+  },
+  {
+    title: "Eat Da Burger",
+    href: "https://eatdaburger-peb.herokuapp.com/",
+    image: "assets/images/burger.jpg",
+    alt: "Eat Da Burger"
+  },
+  {
+    title: "Refassign",
+    href: "https://refassign.herokuapp.com/",
+    image: "assets/images/referee.jpg",
+    alt: "Refassign"
+  },
+  {
+    title: "Scraper Deluxe",
+    href: "https://scraper-peb.herokuapp.com/",
+    image: "assets/images/scraper.jpg",
+    alt: "Scraper Deluxe"
+  },
+  {
+    title: "Memory Game",
+    href: "https://pblack67.github.io/HW15-MemoryGame/",
+    image: "assets/images/shatner.jpg",
+    alt: "Memory Game"
+  }
+];
 
-  module.exports = projects;
+module.exports = projects;
